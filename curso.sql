1fd1c83000dacdd1e2baa8c290e13f3b889ca57e
Socrates5068 <65322699+Socrates5068@users.noreply.github.com>
2021-03-07 20:24:04 -0400

    agregado crud de prestamos

--- curso.sql
+++ curso.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 06-03-2021 a las 02:00:28
--- Versión del servidor: 10.4.17-MariaDB
--- Versión de PHP: 8.0.2
+-- Servidor: localhost:3306
+-- Tiempo de generación: 08-03-2021 a las 00:23:22
+-- Versión del servidor: 5.7.24
+-- Versión de PHP: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `curso`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamos`
+--
+
+CREATE TABLE `prestamos` (
+  `id_prestamo` int(11) NOT NULL,
+  `vehiculo_id` int(11) NOT NULL DEFAULT '0',
+  `usuario_id` int(11) NOT NULL DEFAULT '0',
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `prestamos`
+--
+
+INSERT INTO `prestamos` (`id_prestamo`, `vehiculo_id`, `usuario_id`, `fecha`) VALUES
+(1, 1, 1, '2021-03-07'),
+(2, 1, 3, '2021-03-07'),
+(3, 2, 1, '2021-03-08'),
+(5, 2, 3, '2021-03-06');
 
 -- --------------------------------------------------------
 
@@ -41,9 +64,38 @@ INSERT INTO `users` (`id`, `email`, `password`) VALUES
 (1, 'heraldcnp@gmail.com', '123'),
 (3, 'cliente@gmail.com', '123');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `vehiculos`
+--
+
+CREATE TABLE `vehiculos` (
+  `id_vehiculo` int(11) NOT NULL,
+  `modelo` varchar(20) NOT NULL,
+  `placa` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `vehiculos`
+--
+
+INSERT INTO `vehiculos` (`id_vehiculo`, `modelo`, `placa`) VALUES
+(0, 'Suzuki Swift 2018', 'KFG 456'),
+(1, 'Suzuki Ignis 2019', 'PND 548'),
+(2, 'Suzuki Swace 2020', 'JGT 694');
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD PRIMARY KEY (`id_prestamo`),
+  ADD KEY `vehiculo_id` (`vehiculo_id`),
+  ADD KEY `usuario_id` (`usuario_id`);
 
 --
 -- Indices de la tabla `users`
@@ -52,14 +104,37 @@ ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `vehiculos`
+--
+ALTER TABLE `vehiculos`
+  ADD PRIMARY KEY (`id_vehiculo`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  MODIFY `id_prestamo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`vehiculo_id`) REFERENCES `vehiculos` (`id_vehiculo`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
