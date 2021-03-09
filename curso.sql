0a6a1cc7afeb1fad27e5ba25c8d08047051ac7b4
Socrates5068 <65322699+Socrates5068@users.noreply.github.com>
2021-03-08 21:01:59 -0400

    base de datos arreglada

--- curso.sql
+++ curso.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 08-03-2021 a las 00:23:22
+-- Tiempo de generación: 09-03-2021 a las 01:00:57
 -- Versión del servidor: 5.7.24
 -- Versión de PHP: 7.4.7
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `prestamos` (
   `id_prestamo` int(11) NOT NULL,
-  `vehiculo_id` int(11) NOT NULL DEFAULT '0',
+  `vehiculo_id` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL DEFAULT '0',
   `fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -39,10 +39,8 @@ CREATE TABLE `prestamos` (
 --
 
 INSERT INTO `prestamos` (`id_prestamo`, `vehiculo_id`, `usuario_id`, `fecha`) VALUES
-(1, 1, 1, '2021-03-07'),
-(2, 1, 3, '2021-03-07'),
-(3, 2, 1, '2021-03-08'),
-(5, 2, 3, '2021-03-06');
+(11, 2, 3, '2021-03-09'),
+(12, 8, 4, '2021-03-12');
 
 -- --------------------------------------------------------
 
@@ -62,7 +60,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `email`, `password`) VALUES
 (1, 'heraldcnp@gmail.com', '123'),
-(3, 'cliente@gmail.com', '123');
+(3, 'cliente@gmail.com', '123'),
+(4, 'marco5068@hotmail.com', '5674984');
 
 -- --------------------------------------------------------
 
@@ -73,7 +72,7 @@ INSERT INTO `users` (`id`, `email`, `password`) VALUES
 CREATE TABLE `vehiculos` (
   `id_vehiculo` int(11) NOT NULL,
   `modelo` varchar(20) NOT NULL,
-  `placa` varchar(10) NOT NULL
+  `placa` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -81,9 +80,12 @@ CREATE TABLE `vehiculos` (
 --
 
 INSERT INTO `vehiculos` (`id_vehiculo`, `modelo`, `placa`) VALUES
-(0, 'Suzuki Swift 2018', 'KFG 456'),
-(1, 'Suzuki Ignis 2019', 'PND 548'),
-(2, 'Suzuki Swace 2020', 'JGT 694');
+(1, 'Toyota Corolla 2020', '6581 HTK'),
+(2, 'Suzuki Swace 2018', '8544 PYG'),
+(3, 'Suzuki Ignis 2019', '2148 LGH'),
+(4, 'Suzuki Swift 2018', '3245 PLR'),
+(5, 'Toyota Hilux 2019', '2169 PLT'),
+(8, 'Toyota Etios 2020', '5648 GAS');
 
 --
 -- Índices para tablas volcadas
@@ -95,7 +97,9 @@ INSERT INTO `vehiculos` (`id_vehiculo`, `modelo`, `placa`) VALUES
 ALTER TABLE `prestamos`
   ADD PRIMARY KEY (`id_prestamo`),
   ADD KEY `vehiculo_id` (`vehiculo_id`),
-  ADD KEY `usuario_id` (`usuario_id`);
+  ADD KEY `usuario_id` (`usuario_id`),
+  ADD KEY `vehiculo_id_2` (`vehiculo_id`),
+  ADD KEY `vehiculo_id_3` (`vehiculo_id`);
 
 --
 -- Indices de la tabla `users`
@@ -117,13 +121,19 @@ ALTER TABLE `vehiculos`
 -- AUTO_INCREMENT de la tabla `prestamos`
 --
 ALTER TABLE `prestamos`
-  MODIFY `id_prestamo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_prestamo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `vehiculos`
+--
+ALTER TABLE `vehiculos`
+  MODIFY `id_vehiculo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Restricciones para tablas volcadas
@@ -134,7 +144,7 @@ ALTER TABLE `users`
 --
 ALTER TABLE `prestamos`
   ADD CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `users` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`vehiculo_id`) REFERENCES `vehiculos` (`id_vehiculo`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `prestamos_ibfk_2` FOREIGN KEY (`vehiculo_id`) REFERENCES `vehiculos` (`id_vehiculo`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
